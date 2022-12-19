625d00880196977b1be5e4c031d311235af02e95
Sreekar <84071421+Satya-Sreekar@users.noreply.github.com>
2022-12-19 13:59:26 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            processing
+        </title>
+    </head>
+    <?php
+    $UN = $_POST["UN"];
+    $pd = $_POST["pd"];
+    echo "username= ".$UN."<br>Password= ".$pd;
+    echo "<br><H1>YOU ARE LOGGED IN";
+    ?>
